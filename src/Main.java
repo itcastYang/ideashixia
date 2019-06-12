a4ca4635b51e5fe60ea1ae835c14b8d8bb4d6154
ysheng <2511529678@qq.com>
2019-06-12 20:25:22 +0800

    试试更新

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Main  main = new Main();
+        main.test();
     }
 
     public  void  test(){
